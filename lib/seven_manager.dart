334e24f991a3d7204152a5c54ea66a0775f0a092
Rodrigo <rodrigot-bass@hotmail.com>
2024-07-18 12:55:51 -0300

    Configuração do thema app

--- lib/seven_manager.dart
+++ lib/seven_manager.dart
@@ -1,21 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:seven_manager/src/core/constants/app_routes.dart';
 
+import 'src/core/theme/seven_manager_theme.dart';
 
 class SevenManager extends StatelessWidget {
+  const SevenManager({super.key});
 
-  const SevenManager({ super.key });
-
-   @override
-   Widget build(BuildContext context) {
-       return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-     routes: AppRoutes.router,
-     
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Seven Manager',
+      theme: SevenManagerTheme.lightTheme,
+      routes: AppRoutes.router,
     );
   }
 }
