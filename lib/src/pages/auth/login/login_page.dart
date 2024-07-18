bfaf02c6b3e524795ddd1bff3c4fa36191273eaa
Rodrigo <rodrigot-bass@hotmail.com>
2024-07-18 12:57:22 -0300

    Configuração das cores do app

--- lib/src/pages/auth/login/login_page.dart
+++ lib/src/pages/auth/login/login_page.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
 
-  const LoginPage({ super.key });
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Login'),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                
+                onPressed: () {},
+                child: const Text('Botão'),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  label: Text('Bola'),
+                ),
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+              const Text(
+                'Rodrigo Saymon',
+                style: TextStyle(fontSize: 50),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
